126b54aaa273aea93703150c0e4130d9e6a7bd87
jotaaa22 <joniangamarca333@gmail.com>
2025-02-03 20:36:11 +0100

    updated (calendario)

--- entrada/inicio.php
+++ entrada/inicio.php
@@ -2,8 +2,9 @@
 require_once('C:/Users/jonat/Downloads/xampp/htdocs/recordatorios/funciones/cabecera.php');
 ?>
 <main>
-    <img src="C:\Users\jonat\Downloads\xampp\htdocs\recordatorios\recursos\imagenes\osvaldo.jpeg" width="100px" height="100px">
+    <img src="/../recordatorios/recursos/imagenes/osvaldo.png" width="100px" height="100px">
     <hr>
     <h2>Aquí irá el calendario manin.</h2>
+    <div id='calendar'></div>
 </main>
 </body>
--- funciones/cabecera.php
+++ funciones/cabecera.php
@@ -13,10 +13,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web de recordatorios</title>
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js"></script>
+    <script src="/../recordatorios/recursos/js/calendar.js"></script>
 </head>
 <body>
     <header>
-        <img src="./recursos/imagenes/osvaldo.jpeg" width="100px" height="100px">
+        <img src="/../recordatorios/recursos/imagenes/osvaldo.png" width="100px" height="100px">
         <div id="menu-user">
             <p>Hola <?php echo $_SESSION['email'];?></p>
             <a href="desconectarse.php" id="logout-link">Cerrar sesión</a>
